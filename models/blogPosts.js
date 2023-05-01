985bb609f349ed23ae92ad215c512c96c2220a03
Melanie S <melanie.sapiandante@gmail.com>
2023-05-01 14:46:09 -0400

    created blogPosts-model branch, added code for blogPosts.js referencing project 2 google doc.

---
+++ models/blogPosts.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class blogPosts extends Model { }
+
+blogPosts.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        spotify: {
+            type: DataTypes.STRING,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        comment_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'major', // is this correct? am i reading the diagram in googledrive correctly? 
+            },
+            created_at: {
+                type: DataTypes.DATE,
+                allowNull: false,
+                defaultValue: DataTypes.NOW,
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'project',
+    },
+);
+
+module.exports = blogPosts;
