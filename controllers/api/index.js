561671107b68dd3ff307100dd910d8f2589a3658
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-05-02 11:52:55 -0400

    added user routes, api index routes, and fixed bcrypt

---
+++ controllers/api/index.js
@@ -0,0 +1,18 @@
+//require router and each api route js
+const router = require('express').Router();
+const userRoutes = require('../api/userRoutes');
+const blogRoutes = require('../api/blogRoutes');
+const commentRoutes = require('../api/commentRoutes');
+
+//path to user api
+router.use('/user', userRoutes);
+
+//path to posts api
+router.use('/posts', blogRoutes);
+
+//path to comments api
+router.use('/comments', commentRoutes);
+
+module.exports = router;
+
+
