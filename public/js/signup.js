b670115b91c0b487f2e80b9bfb767d8faee49c0b
Sarita Kharabe <98185682+saritakharabe@users.noreply.github.com>
2023-05-03 11:49:14 -0400

    solved bugs and added school section

--- public/js/signup.js
+++ public/js/signup.js
@@ -9,8 +9,9 @@ const signupFormHandler = async (event) => {
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
+    const schoolName = document.querySelector('#school-signup').value.trim();
   
-    if (username && email && password) {
+    if (username && email && password && schoolName) {
       const response = await fetch('/api/users', {
         method: 'POST',
         body: JSON.stringify({ username, email, password }),
